35a725f5eb2443072a4f91832a754cd160a84a47
Aidan Nabass <ai.nabass@gmail.com>
2024-09-11 10:56:07 -0700

    Setup AttributeMenuWidgetController Delegate
    Setup delegate to broadcast attributeinfo to bound widgets
    Updated widget blueprints to provide variables and functions to receive and update data for attributes.

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,6 +6,12 @@
 #include "UI/Widget/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+
+// Widgets will bind to this to get attribute info
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, AttributeInfo);
+
 /**
  * 
  */
@@ -17,4 +23,11 @@ class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 public:
 	virtual void BroadcastInitialValues() override;
 	virtual void BindCallbacksToDependencies() override;
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
